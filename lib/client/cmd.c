65da7e85c15436738f070dc56c6773d13db1d3b7
xonery <kilian.houpeurt@gmail.com>
2024-10-13 14:26:57 +0200

    fix null ptr for client handler

--- lib/client/cmd.c
+++ lib/client/cmd.c
@@ -33,18 +33,20 @@ unsigned int get_arguments(char *input, char *args[]) {
   return compteur;
 }
 
-unsigned int handle_client_cmd(SOCKET sock, const struct ClientMediator *mediator,
+unsigned int handle_client_cmd(SOCKET sock,
+                               const struct ClientMediator *mediator,
                                char *input) {
   char *args[MAX_ARGS];
 
   const unsigned argslen = get_arguments(input, args);
 
   for (int i = 0; i < MAX_COMMANDS; i++) {
+    const struct ClientHandler *handler = &mediator->handlers[i];
 
-    struct ClientHandler handler = mediator->handlers[i];
-
-    if (strcmp(args[0], handler.prefix) == 0) {
-      return handler.handle(sock, args, argslen);
+    if (handler) {
+      if (strcmp(args[0], handler->prefix) == 0) {
+        return handler->handle(sock, args, argslen);
+      }
     }
   }
   return 0;
